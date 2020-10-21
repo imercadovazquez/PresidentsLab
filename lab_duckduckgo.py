66994f41ecd1a1bb74526e3bf3cd43bbf9f76862
imercadovazquez <imercadovazquez@my.waketech.edu>
2020-10-20 21:25:14 -0400

    first commit

---
+++ lab_duckduckgo.py
@@ -0,0 +1,14 @@
+import requests
+
+
+url_ddg = "https://api.duckduckgo.com/?q=presidents of the united states&format=json&pretty=1"
+resp = requests.get(url_ddg)
+rsp_data = resp.json()
+
+for item in rsp_data:
+    print("item: ", item)
+    print("value:", rsp_data[item])
+    pres_list = rsp_data[item]
+print("\n*****************************************************************\n")
+for x in rsp_data['RelatedTopics']:
+    print("entries: ", x)
---
+++ test_lab_duckduckgo.py
@@ -0,0 +1,123 @@
+import pytest
+import requests
+
+
+url_ddg = "https://api.duckduckgo.com/?q=presidents of the united states&format=json&pretty=1"
+
+
+def test_ddg0():
+    resp = requests.get(url_ddg)
+    rsp_data = resp.json()
+    for item in rsp_data['RelatedTopics']:
+        if "Lincoln" in item['Text']:
+            a = True
+            return
+        else:
+            a = False
+    assert a == True
+
+
+@pytest.mark.parametrize("president", ['Washington', 'Adams', 'Jefferson', 'Madison', 'Monroe'])
+def test_ddg1(president):
+    resp = requests.get(url_ddg)
+    rsp_data = resp.json()
+    for item in rsp_data['RelatedTopics']:
+        if president in item['Text']:
+            a = True
+            return
+        else:
+            a = False
+    assert a == True
+
+
+@pytest.mark.parametrize("president", ['Jackson', 'Van Buren', 'Harrison', 'Tyler', 'Polk'])
+def test_ddg2(president):
+    resp = requests.get(url_ddg)
+    rsp_data = resp.json()
+    for item in rsp_data['RelatedTopics']:
+        if president in item['Text']:
+            a = True
+            return
+        else:
+            a = False
+    assert a == True
+
+
+@pytest.mark.parametrize("president", ['Taylor', 'Fillmore', 'Pierce', 'Buchanan', 'Lincoln'])
+def test_ddg3(president):
+    resp = requests.get(url_ddg)
+    rsp_data = resp.json()
+    for item in rsp_data['RelatedTopics']:
+        if president in item['Text']:
+            a = True
+            return
+        else:
+            a = False
+    assert a == True
+
+
+@pytest.mark.parametrize("president", ['Johnson', 'Grant', 'Hayes', 'Garfield', 'Arthur'])
+def test_ddg4(president):
+    resp = requests.get(url_ddg)
+    rsp_data = resp.json()
+    for item in rsp_data['RelatedTopics']:
+        if president in item['Text']:
+            a = True
+            return
+        else:
+            a = False
+    assert a == True
+
+
+@pytest.mark.parametrize("president", ['Cleveland', 'McKinley', 'Roosevelt', 'Taft', 'Wilson'])
+def test_ddg5(president):
+    resp = requests.get(url_ddg)
+    rsp_data = resp.json()
+    for item in rsp_data['RelatedTopics']:
+        if president in item['Text']:
+            a = True
+            return
+        else:
+            a = False
+    assert a == True
+
+
+@pytest.mark.parametrize("president", ['Harding', 'Coolidge', 'Hoover', 'Truman', 'Eisenhower'])
+def test_ddg6(president):
+    resp = requests.get(url_ddg)
+    rsp_data = resp.json()
+    for item in rsp_data['RelatedTopics']:
+        if president in item['Text']:
+            a = True
+            return
+        else:
+            a = False
+    assert a == True
+
+
+@pytest.mark.parametrize("president", ['Kennedy', 'Nixon', 'Ford', 'Carter', 'Reagan'])
+def test_ddg7(president):
+    resp = requests.get(url_ddg)
+    rsp_data = resp.json()
+    for item in rsp_data['RelatedTopics']:
+        if president in item['Text']:
+            a = True
+            return
+        else:
+            a = False
+    assert a == True
+
+
+@pytest.mark.parametrize("president", ['Bush', 'Clinton', 'Obama', 'Trump'])
+def test_ddg8(president):
+    resp = requests.get(url_ddg)
+    rsp_data = resp.json()
+    for item in rsp_data['RelatedTopics']:
+        if president in item['Text']:
+            a = True
+            return
+        else:
+            a = False
+    assert a == True
+
+
