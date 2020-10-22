76f043a2fe5153bba5eb7db07131a4f1a0523790
Ismael Mercado <71407216+imercadovazquez@users.noreply.github.com>
2020-10-22 14:38:41 -0400

    Update test_lab_duckduckgo.py

--- gitremote/test/test_lab_duckduckgo.py
+++ gitremote/test/test_lab_duckduckgo.py
@@ -16,90 +16,6 @@ def test_ddg0():
     assert a == True
 
 
-@pytest.mark.parametrize("president", ['Washington', 'Adams', 'Jefferson', 'Madison', 'Monroe'])
-def test_ddg1(president):
-    resp = requests.get(url_ddg)
-    rsp_data = resp.json()
-    a = False
-    for item in rsp_data['RelatedTopics']:
-        if president in item['Text']:
-            a = True
-            return            
-    assert a == True
-
-
-@pytest.mark.parametrize("president", ['Jackson', 'Van Buren', 'Harrison', 'Tyler', 'Polk'])
-def test_ddg2(president):
-    resp = requests.get(url_ddg)
-    rsp_data = resp.json()
-    a = False
-    for item in rsp_data['RelatedTopics']:
-        if president in item['Text']:
-            a = True
-            return            
-    assert a == True
-
-
-@pytest.mark.parametrize("president", ['Taylor', 'Fillmore', 'Pierce', 'Buchanan', 'Lincoln'])
-def test_ddg3(president):
-    resp = requests.get(url_ddg)
-    rsp_data = resp.json()
-    a = False
-    for item in rsp_data['RelatedTopics']:
-        if president in item['Text']:
-            a = True
-            return            
-    assert a == True
-
-
-@pytest.mark.parametrize("president", ['Johnson', 'Grant', 'Hayes', 'Garfield', 'Arthur'])
-def test_ddg4(president):
-    resp = requests.get(url_ddg)
-    rsp_data = resp.json()
-    a = False
-    for item in rsp_data['RelatedTopics']:
-        if president in item['Text']:
-            a = True
-            return            
-    assert a == True
-
-
-@pytest.mark.parametrize("president", ['Cleveland', 'McKinley', 'Roosevelt', 'Taft', 'Wilson'])
-def test_ddg5(president):
-    resp = requests.get(url_ddg)
-    rsp_data = resp.json()
-    a = False
-    for item in rsp_data['RelatedTopics']:
-        if president in item['Text']:
-            a = True
-            return            
-    assert a == True
-
-
-@pytest.mark.parametrize("president", ['Harding', 'Coolidge', 'Hoover', 'Truman', 'Eisenhower'])
-def test_ddg6(president):
-    resp = requests.get(url_ddg)
-    rsp_data = resp.json()
-    a = False
-    for item in rsp_data['RelatedTopics']:
-        if president in item['Text']:
-            a = True
-            return            
-    assert a == True
-
-
-@pytest.mark.parametrize("president", ['Kennedy', 'Nixon', 'Ford', 'Carter', 'Reagan'])
-def test_ddg7(president):
-    resp = requests.get(url_ddg)
-    rsp_data = resp.json()
-    a = False
-    for item in rsp_data['RelatedTopics']:
-        if president in item['Text']:
-            a = True
-            return            
-    assert a == True
-
-
 @pytest.mark.parametrize("president", ['Washington', 'Adams', 'Jefferson', 'Madison',
                                   'Monroe', 'Jackson', 'Buren', 'Harrison',
                                   'Tyler', 'Polk', 'Taylor', 'Fillmore', 'Pierce',
@@ -109,7 +25,7 @@ def test_ddg7(president):
                                   'Roosevelt', 'Truman', 'Eisenhower','Kennedy', 'Nixon',
                                   'Ford', 'Carter', 'Reagan', 'Bush', 'Clinton', 'Obama',
                                   'Trump'])
-def test_ddg8(president):
+def test_ddg1(president):
     resp = requests.get(url_ddg)
     rsp_data = resp.json()
     a = False
