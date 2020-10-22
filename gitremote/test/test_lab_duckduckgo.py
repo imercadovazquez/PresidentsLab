7010c3d78267242faed54113ffd5792dad9fdb3d
Ismael Mercado <71407216+imercadovazquez@users.noreply.github.com>
2020-10-22 14:24:22 -0400

    Update test_lab_duckduckgo.py

--- gitremote/test/test_lab_duckduckgo.py
+++ gitremote/test/test_lab_duckduckgo.py
@@ -1,17 +1,6 @@
 import pytest
 import requests
 
-@pytest.mark.parametrize("pres", ['Washington', 'Adams', 'Jefferson', 'Madison',
-                                  'Monroe', 'Adams', 'Jackson', 'Buren', 'Harrison',
-                                  'Tyler', 'Polk', 'Taylor', 'Fillmore', 'Pierce',
-                                  'Buchanan', 'Lincoln', 'Johnson', 'Grant', 'Hayes',
-                                  'Garfield', 'Arthur','Cleveland', 'Harrison',
-                                  'Cleveland', 'McKinley', 'Roosevelt', 'Taft', 'Wilson',
-                                  'Harding', 'Coolidge', 'Hoover', 'Roosevelt', 'Truman',
-                                  'Eisenhower','Kennedy', 'Johnson', 'Nixon', 'Ford',
-                                  'Carter', 'Reagan', 'Bush', 'Clinton','Bush',
-                                  'Obama', 'Trump'])
-
 
 url_ddg = "https://api.duckduckgo.com/?q=presidents of the united states&format=json&pretty=1"
 
